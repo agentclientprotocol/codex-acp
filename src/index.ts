53cef4635e6294fb1399eeac702083a82d44090e
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2025-11-19 15:44:35 +0200

    add acp server example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+#!/usr/bin/env node
+
+import * as acp from "@agentclientprotocol/sdk";
+import {Readable, Writable} from "node:stream";
+import { ExampleAgent } from "./exampleAgent.js";
+
+const input = Writable.toWeb(process.stdout);
+const output = Readable.toWeb(process.stdin) as ReadableStream<Uint8Array>;
+
+const stream = acp.ndJsonStream(input, output);
+new acp.AgentSideConnection((conn) => new ExampleAgent(conn), stream);
